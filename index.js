b16e847c1637dd4760263568033fb404e9688c57
Tanvir Ibn Touhid <tanvirbintouhid5230@gmail.com>
2020-10-15 20:08:07 +0600

    admin client verification added

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+const MongoClient = require("mongodb").MongoClient;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wghoc.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  const adminCollection = client.db(process.env.DB_NAME).collection("admins");
+  app.post("/checkuser", (req, res) => {
+    const email = req.body.email;
+    adminCollection.findOne({ email }, (err, result) => {
+      if (result) {
+        res.send({ userType: "admin" });
+        console.log("admin");
+      } else {
+        res.send({ userType: "client" });
+        console.log("client");
+      }
+    });
+  });
+});
+
+app.listen(port);
